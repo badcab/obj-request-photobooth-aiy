65ef9144441bde08a5579899a4bf58f0d9deb2e3
Michael Spear <badcab@bsd4breakfast.us>
2018-05-28 17:13:41 -0500

    added colom

--- rand_photo_requirements.py
+++ rand_photo_requirements.py
@@ -1,6 +1,6 @@
 class Rand_photo_req:
 
-	def get_new_photo_requirement(self) 
+	def get_new_photo_requirement(self): 
 		#get random items
 		return '2 people smiling'
 		#maybe return a data structure
