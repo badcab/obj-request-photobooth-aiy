acf86785edf246d7574a777490025c406a100100
Michael Spear <badcab@bsd4breakfast.us>
2018-05-28 17:21:18 -0500

    added colom

--- object_detection_module.py
+++ object_detection_module.py
@@ -16,12 +16,12 @@ class Object_detection_module:
 		return image.crop((x, y, x + size, y + size)), (x, y)
 
 	def get_on_screen(self):
-		with ImageInference(object_detection.model()) as inference:
-			image = Image.open(
-				io.BytesIO(sys.stdin.buffer.read())
+		with self.ImageInference(object_detection.model()) as inference:
+			image = self.Image.open(
+				self.io.BytesIO(self.sys.stdin.buffer.read())
 				if args.input == '-' else args.input)
 			image_center, offset = self._crop_center(image)
-			draw = ImageDraw.Draw(image)
+			draw = self.ImageDraw.Draw(image)
 			result = inference.run(image_center)
 			for i, obj in enumerate(object_detection.get_objects(result, 0.3, offset)):
 				print('Object #%d: %s' % (i, str(obj)))
