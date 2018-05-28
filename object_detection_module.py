419a8c392ed3f000ce6ce2ef9b339350bcff1ec6
Michael Spear <badcab@bsd4breakfast.us>
2018-05-28 18:38:59 -0500

    maybe this method will work

--- object_detection_module.py
+++ object_detection_module.py
@@ -1,40 +1,21 @@
 class Object_detection_module:
-
-	import argparse
-	import io
-	import sys
-	from PIL import Image
-	from PIL import ImageDraw
-
+	from picamera import PiCamera
 	from aiy.vision.inference import ImageInference
 	from aiy.vision.models import object_detection
-	
-	def _crop_center(self, image): #@TODO remove depencancy on this
-		width, height = image.size
-		size = min(width, height)
-		x, y = (width - size) / 2, (height - size) / 2
-		return image.crop((x, y, x + size, y + size)), (x, y)
 
 	def get_on_screen(self):
-	
-		#parser = self.argparse.ArgumentParser()
-		#parser.add_argument('--input', '-i', dest='input', required=True)
-		#parser.add_argument('--output', '-o', dest='output')
-		#args = parser.parse_args()
-		
-		with self.ImageInference(self.object_detection.model()) as inference:
-			image = self.Image.open(
-				self.io.BytesIO(self.sys.stdin.buffer.read())
-				#if args.input == '-' else args.input
-				
-				)
-			image_center, offset = self._crop_center(image)
-			draw = self.ImageDraw.Draw(image)
-			result = inference.run(image_center)
-			for i, obj in enumerate(object_detection.get_objects(result, 0.3, offset)):
-				print('Object #%d: %s' % (i, str(obj)))
-			
-			return ['banana', 'face'] #@TODO make this actually work
+		with self.PiCamera() as camera:
+			camera.sensor_mode = 4
+			camera.resolution = (1640, 922)  #  1232 is Full height (Camera v2), 922 is 16:9 
+			camera.framerate = 30
+
+			with self.CameraInference(self.object_detection.model()) as inference:
+				result = inference.run()
+				for i, obj in enumerate(object_detection.get_objects(result, 0.3)):
+					print('Object #%d: %s' % (i, str(obj)))
+
+				camera.stop_preview() #to clean it up
+				return ['banana', 'face'] #@TODO make this actually work
 
 
 #goal is to just get a list of things on the screen
