815868160e64901596af030210829e080781f1f3
Michael Spear <badcab@bsd4breakfast.us>
2018-05-28 11:25:35 -0500

    a few more things

--- led_module.py
+++ led_module.py
@@ -6,37 +6,57 @@ from aiy.leds import Pattern
 from aiy.leds import PrivacyLed
 from aiy.leds import RgbLeds
 
-RED = (0xFF, 0x00, 0x00)
-GREEN = (0x00, 0xFF, 0x00)
-YELLOW = (0xFF, 0xFF, 0x00)
-BLUE = (0x00, 0x00, 0xFF)
-PURPLE = (0xFF, 0x00, 0xFF)
-CYAN = (0x00, 0xFF, 0xFF)
-WHITE = (0xFF, 0xFF, 0xFF)
-OFF = (0x00, 0xFF, 0xFF)
+class Led_module:
+	RED = (0xFF, 0x00, 0x00)
+	GREEN = (0x00, 0xFF, 0x00)
+	YELLOW = (0xFF, 0xFF, 0x00)
+	BLUE = (0x00, 0x00, 0xFF)
+	PURPLE = (0xFF, 0x00, 0xFF)
+	CYAN = (0x00, 0xFF, 0xFF)
+	WHITE = (0xFF, 0xFF, 0xFF)
+	OFF = (0x00, 0xFF, 0xFF)
 
-leds = Leds()
+	leds = Leds()
 
-def set_led_red
-	leds.update(Leds.reg_on(RED))
-	
-def set_led_yellow
-	leds.update(Leds.reg_on(YELLOW))
-	
-def set_led_green
-	leds.update(Leds.reg_on(GREEN))
-	
-def set_led_off
-	leds.update(Leds.rgb_off())
-	
-def set_privacy_on
-	leds.update(Leds.privacy_on())
+	def __init__(self, verbose):
+		self.verbose = bool(verbose)
 
-def set_privacy_off
-	leds.update(Leds.privacy_off())
-	
-def set_led_blink
-	leds.pattern = Pattern.blink(500)
-	
-def reset_led
-	leds.reset()
+	def set_led_red():
+		leds.update(Leds.reg_on(RED))
+		if(self.verbose):
+			print('red')
+
+	def set_led_yellow():
+		leds.update(Leds.reg_on(YELLOW))
+		if(self.verbose):
+			print('yellow')
+
+	def set_led_green():
+		leds.update(Leds.reg_on(GREEN))
+		if(self.verbose):
+			print('green')
+
+	def set_led_off():
+		leds.update(Leds.rgb_off())
+		if(self.verbose):
+			print('off')
+
+	def set_privacy_on():
+		leds.update(Leds.privacy_on())
+		if(self.verbose):
+			print('p on')
+
+	def set_privacy_off():
+		leds.update(Leds.privacy_off())
+		if(self.verbose):
+			print('p off')
+
+	def set_led_blink():
+		leds.pattern = Pattern.blink(500)
+		if(self.verbose):
+			p('blink')
+
+	def reset_led():
+		leds.reset()
+		if(self.verbose):
+			print('reset')
--- object_request_photobooth.py
+++ object_request_photobooth.py
@@ -2,3 +2,16 @@
 #trigger camera when that condition is met
 
 #for starters let us trigger a picture based on a static requirment
+
+import led_module
+
+def main():
+	lights = led_module.Led_module(1)
+
+	lights.set_led_red()
+	sleep(5)
+
+
+
+if __name__ == '__main__':
+	main()
