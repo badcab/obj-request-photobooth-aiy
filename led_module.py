2aa32d0deb3332c8c451f00c1eecbbd3ca1e6cf1
Michael Spear <badcab@bsd4breakfast.us>
2018-05-28 14:48:04 -0500

    opps

--- led_module.py
+++ led_module.py
@@ -1,12 +1,14 @@
 import math
 import time
 
-from aiy.vision.leds import Leds
-from aiy.vision.leds import Pattern
-from aiy.vision.leds import PrivacyLed
-from aiy.vision.leds import RgbLeds
+
 
 class Led_module:
+	from aiy.vision.leds import Leds
+	from aiy.vision.leds import Pattern
+	from aiy.vision.leds import PrivacyLed
+	from aiy.vision.leds import RgbLeds
+
 	RED = (0xFF, 0x00, 0x00)
 	GREEN = (0x00, 0xFF, 0x00)
 	YELLOW = (0xFF, 0xFF, 0x00)
@@ -26,37 +28,37 @@ class Led_module:
 		if(self.verbose):
 			print(__name__)
 
-	def set_led_yellow():
+	def set_led_yellow(self):
 		leds.update(Leds.reg_on(YELLOW))
 		if(self.verbose):
 			print(__name__)
 
-	def set_led_green():
+	def set_led_green(self):
 		leds.update(Leds.reg_on(GREEN))
 		if(self.verbose):
 			print(__name__)
 
-	def set_led_off():
+	def set_led_off(self):
 		leds.update(Leds.rgb_off())
 		if(self.verbose):
 			print(__name__)
 
-	def set_privacy_on():
+	def set_privacy_on(self):
 		leds.update(Leds.privacy_on())
 		if(self.verbose):
 			print(__name__)
 
-	def set_privacy_off():
+	def set_privacy_off(self):
 		leds.update(Leds.privacy_off())
 		if(self.verbose):
 			print(__name__)
 
-	def set_led_blink():
+	def set_led_blink(self):
 		leds.pattern = Pattern.blink(500)
 		if(self.verbose):
 			p(__name__)
 
-	def reset_led():
+	def reset_led(self):
 		leds.reset()
 		if(self.verbose):
 			print(__name__)
