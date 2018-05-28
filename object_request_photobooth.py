267c6d090c14cee66ec47cb2c707151b03dde9ea
Michael Spear <badcab@bsd4breakfast.us>
2018-05-28 15:51:49 -0500

    putting down the plumbing

--- object_request_photobooth.py
+++ object_request_photobooth.py
@@ -9,6 +9,8 @@ import sound_module
 def main():
 	lights = led_module.Led_module()
 	sound = sound_module.Sound_module()
+	detect = object_detection_module.Object_detection_module()
+	req = rand_photo_requirements.Rand_photo_req()
 
 	sound.shutter()
 	lights.set_led_red()
@@ -21,6 +23,19 @@ def main():
 	time.sleep(1)
 	lights.set_led_off()
 	sound.blip()
+	
+
+	while 1 == 1: #@TODO add a breakin feature for if the btn on top is pressed
+		requirement = req.get_new_photo_requirement()
+		while requirement != detect.get_on_screen(): #@TODO change to be contains
+			time.sleep(1) #slows things down a bit
+			#determine the color of the led
+		#end while
+		lights.set_led_green()
+		lights.set_led_blink()
+		#take the actual picture here
+		sound.shutter()
+		
 
 
 if __name__ == '__main__':
